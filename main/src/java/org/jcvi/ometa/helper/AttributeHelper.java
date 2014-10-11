4ae8ed06a600c5df8ab89ad68f0dd8abbe970b75
Hyunsoo Daniel Kim <hkim500@gmail.com>
2014-10-11 00:58:38 -0400

    get project/sample attributes when there is no event

--- main/src/java/org/jcvi/ometa/helper/AttributeHelper.java
+++ main/src/java/org/jcvi/ometa/helper/AttributeHelper.java
@@ -50,66 +50,69 @@ public class AttributeHelper {
                     Event currEvent = readPersister.getLatestEventForSample(projectId, isProject ? null : currSample.getSampleId(), eventId);
 
                     List<FileReadAttributeBean> beanList = new ArrayList<FileReadAttributeBean>();
-                    if(currEvent != null) {
+
+                    Map<Long, EventAttribute> eaMap = new HashMap<Long, EventAttribute>();
+                    if(currEvent != null) { //there could be no event, then move on to project/sample attributes
                         //get the latest event attributes
                         List<EventAttribute> eaList = readPersister.getEventAttributes(currEvent.getEventId(), projectId);
-                        Map<Long, EventAttribute> eaMap = new HashMap<Long, EventAttribute>(eaList.size());
                         for(EventAttribute ea : eaList) {
                             eaMap.put(ea.getMetaAttribute().getLookupValue().getLookupValueId(), ea);
                         }
+                    }
 
-                        //get sample attributes
-                        List<ProjectAttribute> paList = null;
-                        Map<Long, ProjectAttribute> paMap = null;
-                        List<SampleAttribute> saList = null;
-                        Map<Long, SampleAttribute> saMap = null;
-                        if(isProject) {
-                            paList = readPersister.getProjectAttributes(projectId);
-                            paMap = new HashMap<Long, ProjectAttribute>(paList.size());
-                            for(ProjectAttribute pa : paList) {
-                                paMap.put(pa.getMetaAttribute().getLookupValue().getLookupValueId(), pa);
-                            }
+                    //get project/sample attributes
+                    List<ProjectAttribute> paList = null;
+                    Map<Long, ProjectAttribute> paMap = null;
+                    List<SampleAttribute> saList = null;
+                    Map<Long, SampleAttribute> saMap = null;
+                    if(isProject) {
+                        paList = readPersister.getProjectAttributes(projectId);
+                        paMap = new HashMap<Long, ProjectAttribute>(paList.size());
+                        for(ProjectAttribute pa : paList) {
+                            paMap.put(pa.getMetaAttribute().getLookupValue().getLookupValueId(), pa);
+                        }
+                    } else {
+                        saList = readPersister.getSampleAttributes(currSample.getSampleId());
+                        saMap = new HashMap<Long, SampleAttribute>(saList.size());
+                        for(SampleAttribute sa : saList) {
+                            saMap.put(sa.getMetaAttribute().getLookupValue().getLookupValueId(), sa);
+                        }
+                    }
+
+                    List<EventMetaAttribute> emaList = readPersister.getEventMetaAttributes(projectId, eventId);
+                    for(EventMetaAttribute ema : emaList) {
+                        FileReadAttributeBean frBean = new FileReadAttributeBean();
+                        frBean.setAttributeName(ema.getLookupValue().getName());
+
+                        AttributeModelBean attributeModelBean = null;
+                        Long emaLookupValueId = ema.getLookupValue().getLookupValueId();
+                        if(eaMap.containsKey(emaLookupValueId)) { //get the latest event attribute value
+                            attributeModelBean = eaMap.get(emaLookupValueId);
                         } else {
-                            saList = readPersister.getSampleAttributes(currSample.getSampleId());
-                            saMap = new HashMap<Long, SampleAttribute>(saList.size());
-                            for(SampleAttribute sa : saList) {
-                                saMap.put(sa.getMetaAttribute().getLookupValue().getLookupValueId(), sa);
-                            }
-                        }
-
-                        List<EventMetaAttribute> emaList = readPersister.getEventMetaAttributes(projectId, eventId);
-                        for(EventMetaAttribute ema : emaList) {
-                            FileReadAttributeBean frBean = new FileReadAttributeBean();
-                            frBean.setAttributeName(ema.getLookupValue().getName());
-
-                            AttributeModelBean attributeModelBean = null;
-                            Long emaLookupValueId = ema.getLookupValue().getLookupValueId();
-                            if(eaMap.containsKey(emaLookupValueId)) { //get the latest event attribute value
-                                attributeModelBean = eaMap.get(emaLookupValueId);
-                            } else {
-                                if(isProject && paMap.containsKey(emaLookupValueId)) {
+                            if(isProject) {
+                                if(paMap != null && paMap.containsKey(emaLookupValueId)) {
                                     attributeModelBean = paMap.get(emaLookupValueId);
-                                } else {
-                                    if(saMap != null && saMap.containsKey(emaLookupValueId)) { //get sample attribute value if event attribute has no record
-                                        attributeModelBean = saMap.get(emaLookupValueId);
-                                    }
+                                }
+                            } else {
+                                if(saMap != null && saMap.containsKey(emaLookupValueId)) { //get sample attribute value if event attribute has no record
+                                    attributeModelBean = saMap.get(emaLookupValueId);
                                 }
                             }
-
-                            Object attributeValue = "";
-                            if(attributeModelBean != null) {
-                                attributeValue = ModelValidator.getModelValue(ema.getLookupValue(), attributeModelBean);
-                                if(attributeValue != null) {
-                                    if(attributeValue.getClass() == Date.class || attributeValue.getClass() == Timestamp.class) {
-                                        attributeValue = CommonTool.convertTimestampToDate(attributeValue);
-                                    }
-                                }
-                            }
-                            frBean.setAttributeValue("" + attributeValue);
-                            frBean.setSampleName(currSample == null ? null : currSample.getSampleName());
-                            frBean.setProjectName(currProject.getProjectName());
-                            beanList.add(frBean);
                         }
+
+                        Object attributeValue = "";
+                        if(attributeModelBean != null) {
+                            attributeValue = ModelValidator.getModelValue(ema.getLookupValue(), attributeModelBean);
+                            if(attributeValue != null) {
+                                if(attributeValue.getClass() == Date.class || attributeValue.getClass() == Timestamp.class) {
+                                    attributeValue = CommonTool.convertTimestampToDate(attributeValue);
+                                }
+                            }
+                        }
+                        frBean.setAttributeValue("" + attributeValue);
+                        frBean.setSampleName(currSample == null ? null : currSample.getSampleName());
+                        frBean.setProjectName(currProject.getProjectName());
+                        beanList.add(frBean);
                     }
 
                     AttributePair pair = new AttributePair();
