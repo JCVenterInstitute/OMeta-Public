822341be1c98eef34e2866cadc8dd17af460d1bc
Mehmet Kuscuoglu <mkuscuog@jcvi.org>
2015-07-08 10:44:15 -0400

    add Dictionary & DictionaryDependency model and dao classes

---
+++ main/src/java/org/jcvi/ometa/hibernate/dao/DictionaryDAO.java
@@ -0,0 +1,158 @@
+package org.jcvi.ometa.hibernate.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.criterion.*;
+import org.jcvi.ometa.model.Dictionary;
+import org.jcvi.ometa.model.DictionaryDependency;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by mkuscuog on 6/23/2015.
+ */
+public class DictionaryDAO extends HibernateDAO {
+
+    public List<Dictionary> getDictionaries(Session session) throws DAOException {
+        List<Dictionary> rtnVal = null;
+
+        try {
+            Criteria crit = session.createCriteria( Dictionary.class );
+            rtnVal = crit.list();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public List<DictionaryDependency> getDictionaryDependencies(Session session) throws DAOException {
+        List<DictionaryDependency> rtnVal = null;
+
+        try {
+            Criteria crit = session.createCriteria( DictionaryDependency.class );
+            rtnVal = crit.list();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public List<Dictionary> getDictionaryByType( String dictType, Session session ) throws DAOException {
+        List<Dictionary> rtnVal = null;
+
+        try {
+            Criteria crit = session.createCriteria( Dictionary.class );
+            crit.add( Restrictions.eq("dictionaryType", dictType) );
+            rtnVal = crit.list();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public Dictionary getDictionaryByTypeAndCode(String dictType, String dictCode, Session session) throws DAOException {
+        Dictionary rtnVal = null;
+
+        try {
+            Criteria crit = session.createCriteria( Dictionary.class );
+            crit.add( Restrictions.eq("dictionaryType", dictType) );
+            crit.add( Restrictions.eq("dictionaryCode", dictCode) );
+            rtnVal = (Dictionary) crit.uniqueResult();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public List<Dictionary> getDictionaryDependenciesByType( String dictType, String dictCode, Session session ) throws DAOException {
+        List<Dictionary> rtnVal = null;
+
+        try {
+            String sql = " select distinct * from ifx_projects.dictionary d " +
+                    "where d.dict_id in (" +
+                    "select dd.dict_id from ifx_projects.dictionary_dependency dd " +
+                    "left join ifx_projects.dictionary d on dd.parent_id = d.dict_id " +
+                    "where d.dict_type = :dictType and d.dict_code = :dictCode)";
+
+            SQLQuery query = session.createSQLQuery(sql);
+            query.addEntity(Dictionary.class);
+            query.setParameter("dictType", dictType);
+            query.setParameter("dictCode", dictCode);
+
+            rtnVal = query.list();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public List<Object[]> getAllDictionaryTypeCodePairs(Session session) throws DAOException {
+        List<Object[]> rtnVal = null;
+
+        try {
+            ProjectionList projList = Projections.projectionList();
+            projList.add(Projections.groupProperty("dictionaryType"));
+            projList.add(Projections.groupProperty("dictionaryCode"));
+
+            Criteria crit = session.createCriteria( Dictionary.class );
+            crit.setProjection(projList);
+            rtnVal = (List<Object[]>) crit.list();
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+
+        return rtnVal;
+    }
+
+    public void loadDictionary(String dictType, String dictValue, String dictCode, Date creationDate, Session session) throws DAOException {
+        try {
+            Dictionary dictionary = new Dictionary();
+            dictionary.setDictionaryCode(dictCode);
+            dictionary.setDictionaryValue(dictValue);
+            dictionary.setDictionaryType(dictType);
+            dictionary.setCreationDate(creationDate);
+
+            session.save(dictionary);
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+    }
+
+    public void loadDictionaryWithDependency(String dictType, String dictValue, String dictCode,
+                                             String parentDictTypeCode, Date creationDate, Session session) throws DAOException {
+        try {
+            Dictionary dictionary = new Dictionary();
+            dictionary.setDictionaryCode(dictCode);
+            dictionary.setDictionaryValue(dictValue);
+            dictionary.setDictionaryType(dictType);
+            dictionary.setCreationDate(creationDate);
+
+            session.save(dictionary);
+
+            String[] dictTypeCode = parentDictTypeCode.split(" - ");
+
+            Criteria crit = session.createCriteria(Dictionary.class);
+            crit.add(Restrictions.eq("dictionaryType", dictTypeCode[0]));
+            crit.add(Restrictions.eq("dictionaryCode", dictTypeCode[1]));
+            crit.setProjection(Projections.property("dictionaryId"));
+
+            Long parentDictId = (Long) crit.uniqueResult();
+
+            DictionaryDependency dictDependency = new DictionaryDependency();
+            dictDependency.setParentId(parentDictId);
+            dictDependency.setDictionaryId(dictionary.getDictionaryId());
+            dictDependency.setCreatedDate(creationDate);
+
+            session.save(dictDependency);
+        } catch ( Exception ex ) {
+            throw new DAOException( ex );
+        }
+    }
+}
---
+++ main/src/java/org/jcvi/ometa/model/Dictionary.java
@@ -0,0 +1,112 @@
+package org.jcvi.ometa.model;
+
+import org.jcvi.ometa.configuration.JCVI_BeanPopulator_Column;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by mkuscuog on 6/23/2015.
+ */
+@Entity
+@Table(name="dictionary")
+public class Dictionary implements ModelBean {
+
+    private Long dictionaryId;
+    private String dictionaryType;
+    private String dictionaryCode;
+    private String dictionaryValue;
+
+    private Date creationDate;
+    private Date modifiedDate;
+    private Long createdBy;
+    private Long modifiedBy;
+    private String parentDependency;
+
+    @Id
+    @Column(name="dict_id", nullable=false)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public Long getDictionaryId() {
+        return dictionaryId;
+    }
+
+    public void setDictionaryId(Long dictionaryId) {
+        this.dictionaryId = dictionaryId;
+    }
+
+    @Column(name="dict_type", nullable=false)
+    public String getDictionaryType() {
+        return dictionaryType;
+    }
+
+    @JCVI_BeanPopulator_Column("DictionaryType")
+    public void setDictionaryType(String dictionaryType) {
+        this.dictionaryType = dictionaryType;
+    }
+
+    @Column(name="dict_code", nullable=false)
+    public String getDictionaryCode() {
+        return dictionaryCode;
+    }
+
+    @JCVI_BeanPopulator_Column("DictionaryCode")
+    public void setDictionaryCode(String dictionaryCode) {
+        this.dictionaryCode = dictionaryCode;
+    }
+
+    @Column(name="dict_value", nullable=false)
+    public String getDictionaryValue() {
+        return dictionaryValue;
+    }
+
+    @JCVI_BeanPopulator_Column("DictionaryValue")
+    public void setDictionaryValue(String dictionaryValue) {
+        this.dictionaryValue = dictionaryValue;
+    }
+
+    @Column(name="dict_create_date", nullable=false)
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Column(name="dict_modify_date")
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    @Transient
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Transient
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Transient
+    public String getParentDependency() {
+        return parentDependency;
+    }
+
+    @JCVI_BeanPopulator_Column("ParentDictionary")
+    public void setParentDependency(String parentDependency) {
+        this.parentDependency = parentDependency;
+    }
+}
